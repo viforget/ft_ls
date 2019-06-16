74a90027afb2d6f4ce26b19cd4df3cab7ea982ec
Tchoupe <ntom@student.s19.be>
2019-06-16 19:12:51 +0200

    parsing fini et .h commence

---
+++ includes/ft_ls.h
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/16 18:04:05 by ntom              #+#    #+#             */
+/*   Updated: 2019/06/16 18:52:32 by ntom             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+/*
+** LIBRARIES
+*/
+
+# include "../libft/libft.h"
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <pwd.h>
+# include <dirent.h>
+# include <time.h>
+# include <grp.h>
+# include <sys/acl.h>
+# include <sys/xattr.h>
+# include <stdio.h>
+# include <uuid/uuid.h>
+
+/*
+** MACRO
+*/
+
+# define ON 1
+# define OFF 0
+# define OPT_L 0
+# define OPT_MR 1
+# define OPT_A 2
+# define OPT_R 3
+# define OPT_T 4
+
+
+/*
+** STRUCTURES
+*/
+
+typedef struct		s_info
+{
+	char 			*path;
+	char			*name;
+	struct stat		stats;
+	struct s_info	*left;
+	struct s_info	*right;
+}					t_info;
+
+/*
+** MAIN.C
+*/
+
+#endif
---
+++ main.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/16 18:20:05 by ntom              #+#    #+#             */
+/*   Updated: 2019/06/16 19:12:30 by ntom             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/ft_ls.h"
+
+static int	ft_option(char *str, int *flags)
+{
+	static char		*flag = "lRart";
+	int				i;
+	int				j;
+
+	j = 1;
+	while (str[j])
+	{
+		i = 0;
+		while (flag[i])
+		{
+			if (str[j] == flag[i])
+			{
+				*flags |= ON << i;
+				break ;
+			}
+			i++;
+			if (flag[i] == '\0')
+				return (printf("ft_ls: illegal option -- %c\n", str[j]));
+		}
+		j++;
+	}
+	return (0);
+}
+
+static int	parsing(char **argv, int *flags)
+{
+	int		i;
+
+	i = 1;
+	while (argv[i])
+	{
+		if ((argv[i][0] == '-' && argv[i][1] == '\0') || argv[i][0] != '-')
+			break ;
+		if (argv[i][0] == '-' && argv[i][1] == '-' && argv[i][2] == '\0')
+		{
+			i++;
+			break ;
+		}
+		if (argv[i][0] == '-')
+			ft_option(argv[i], flags);
+		i++;
+	}
+	return (i);
+}
+
+int			main(int argc, char **argv)
+{
+	int		i;
+	int		flags;
+
+	i = parsing(argv, &flags);
+	if (argv[i] == NULL)
+		;
+	return (0);
+}
