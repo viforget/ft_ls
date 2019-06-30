6788fe379189f638592a50e8e5df90f69ca1a144
Tchoupe <ntom@student.s19.be>
2019-06-30 18:29:54 +0200

    link uid group added

--- aff_del_tree.c
+++ aff_del_tree.c
@@ -6,7 +6,7 @@
 /*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 17:24:49 by ntom              #+#    #+#             */
-/*   Updated: 2019/06/30 17:39:10 by ntom             ###   ########.fr       */
+/*   Updated: 2019/06/30 18:27:50 by ntom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		del_tree(t_info *tree)
 			&& ft_strcmp(tree->name, "..") != 0
 			&& (is_on(g_flags, OPT_A) || tree->name[0] != '.'))
 	{
-		put_mult_str(3, "\n", tree->path, ":");
+		put_mult_str(4, "\n", tree->path, ":", "\n");
 		ft_ls(tree->name, tree->path);
 	}
 	if (tree->right != NULL)
@@ -39,7 +39,15 @@ void		aff_tree(t_info *tree)
 	if (is_on(g_flags, OPT_A) || tree->name[0] != '.')
 	{
 		if (is_on(g_flags, OPT_L))
+		{
 			ft_putstr(tree->ftr);
+			if (tree->stats.st_nlink < 10)
+				ft_putchar(' ');
+			ft_putchar(' ');
+			ft_putnbr(tree->stats.st_nlink);
+			put_mult_str(5, " ", getpwuid(tree->stats.st_uid)->pw_name, "  "
+			, getgrgid(tree->stats.st_gid)->gr_name, " ");
+		}
 		ft_putendl(tree->name);
 	}
 	if (tree->right != NULL)
--- libft/ft_putmultstr.c
+++ libft/ft_putmultstr.c
@@ -6,7 +6,7 @@
 /*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 23:25:47 by ntom              #+#    #+#             */
-/*   Updated: 2019/06/21 23:55:05 by ntom             ###   ########.fr       */
+/*   Updated: 2019/06/30 18:00:24 by ntom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,7 @@ int			put_mult_str(int num, ...)
 	while (num != 0)
 	{
 		tmp = va_arg(ap, char *);
-		if (num == 1)
-			ft_putendl(tmp);
-		else
-			ft_putstr(tmp);
+		ft_putstr(tmp);
 		num--;
 	}
 	va_end(ap);
