77fad46d714b47e57df96acf710e87a23f87ca44
Tchoupe <ntom@student.s19.be>
2019-07-10 14:56:32 +0200

    grid et uid Users handled

--- aff_del_tree.c
+++ aff_del_tree.c
@@ -6,7 +6,7 @@
 /*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 17:24:49 by ntom              #+#    #+#             */
-/*   Updated: 2019/06/30 18:27:50 by ntom             ###   ########.fr       */
+/*   Updated: 2019/07/10 14:54:47 by ntom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,38 @@ void		del_tree(t_info *tree)
 	free(tree);
 }
 
+static void		aff_l(t_info *tree)
+{
+	ft_putstr(tree->ftr);
+	if (tree->stats.st_nlink < 10)
+		ft_putchar(' ');
+	ft_putchar(' ');
+	ft_putnbr(tree->stats.st_nlink);
+	ft_putchar(' ');
+	if (getpwuid(tree->stats.st_uid) == NULL)
+	{
+		ft_putnbr(tree->stats.st_uid);
+		ft_putchar(' ');
+	}
+	else
+		put_mult_str(2, getpwuid(tree->stats.st_uid)->pw_name, "  ");
+	if (getgrgid(tree->stats.st_gid) == NULL)
+	{
+		ft_putnbr(tree->stats.st_gid);
+		ft_putchar(' ');
+	}
+	else
+		put_mult_str(2, getgrgid(tree->stats.st_gid)->gr_name, " ");
+	if (tree->ftr[0] != 'c' && tree->ftr[0] != 'b')
+		ft_putnbr(tree->stats.st_size);
+	else
+	{
+		ft_putnbr(major(tree->stats.st_rdev));
+		ft_putnbr(minor(tree->stats.st_rdev));
+	}
+	ft_putchar(' ');
+}
+
 void		aff_tree(t_info *tree)
 {
 	if (tree->left != NULL)
@@ -39,15 +71,7 @@ void		aff_tree(t_info *tree)
 	if (is_on(g_flags, OPT_A) || tree->name[0] != '.')
 	{
 		if (is_on(g_flags, OPT_L))
-		{
-			ft_putstr(tree->ftr);
-			if (tree->stats.st_nlink < 10)
-				ft_putchar(' ');
-			ft_putchar(' ');
-			ft_putnbr(tree->stats.st_nlink);
-			put_mult_str(5, " ", getpwuid(tree->stats.st_uid)->pw_name, "  "
-			, getgrgid(tree->stats.st_gid)->gr_name, " ");
-		}
+			aff_l(tree);
 		ft_putendl(tree->name);
 	}
 	if (tree->right != NULL)
