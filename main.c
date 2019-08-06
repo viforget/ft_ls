68b7577f90d5281b45447591e571eafe8a737260
Tchoupe <ntom@student.s19.be>
2019-08-06 16:26:28 +0200

    fix for merge

--- main.c
+++ main.c
@@ -6,90 +6,12 @@
 /*   By: ntom <ntom@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/16 18:20:05 by ntom              #+#    #+#             */
-/*   Updated: 2019/08/06 16:03:57 by viforget         ###   ########.fr       */
+/*   Updated: 2019/08/06 16:25:41 by ntom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_ls.h"
 
-/*static int		ft_isdir(char *current)
-{
-	struct stat		buf;
-
-	if (lstat(current, &buf) == -1)
-		return (0);
-	return (S_ISDIR(buf.st_mode));
-}
-
-static int		compare(char *current, char *next)
-{
-	struct stat		buf;
-	int				ret_current;
-	int				ret_next;
-
-	ret_current = lstat(current, &buf);
-	ret_next = lstat(next, &buf);
-	if (ret_current < ret_next)
-		return (-1);
-	else if (ret_current > ret_next)
-		return (1);
-	if (ft_isdir(current) && ft_isdir(next) == 0)
-		return (1);
-	else if (ft_isdir(next) && ft_isdir(current) == 0)
-		return (-1);
-	return (ft_strcmp(current, next));
-}
-
-static int		r_compare(char *current, char *next)
-{
-	struct stat		buf;
-	int				ret_current;
-	int				ret_next;
-
-	ret_current = lstat(current, &buf);
-	ret_next = lstat(next, &buf);
-	if (ret_current < ret_next)
-		return (-1);
-	else if (ret_current > ret_next)
-		return (1);
-	if (ft_isdir(current) && ft_isdir(next) == 0)
-		return (1);
-	else if (ft_isdir(next) && ft_isdir(current) == 0)
-		return (-1);
-	else if (ret_current == -1)
-		return (ft_strcmp(current, next));
-	return (-ft_strcmp(current, next));
-}
-
-static void		sort_arg(char **argv, int argc)
-{
-	int				i;
-
-	i = 0;
-	while (i < argc)
-	{
-		if (argv[i][0] == '\0')
-			return (ft_putendl("ft_ls: fts_open: No such file or directory"));
-		if (argv[i + 1]
-			&& compare(argv[i], argv[i + 1]) > 0
-			&& is_off(g_flags, OPT_R))
-		{
-			ft_strswap(&argv[i], &argv[i + 1]);
-			i = 0;
-			continue ;
-		}
-		else if (argv[i + 1]
-			&& r_compare(argv[i], argv[i + 1]) > 0
-			&& is_on(g_flags, OPT_R))
-		{
-			ft_strswap(&argv[i], &argv[i + 1]);
-			i = 0;
-			continue ;
-		}
-		i++;
-	}
-}
-*/
 void			ft_ls(char *st, char *path)
 {
 	t_info			*tree;
