b7ef68684b4a1557ec5e3196c68b7299a82ffcea
Victor Forget <viforget@student.s19.be>
2019-08-06 21:18:58 +0200

    Dirr OK + le pire booleen du monde

--- argv_sort.c
+++ argv_sort.c
@@ -6,7 +6,7 @@
 /*   By: viforget <viforget@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/04 17:51:39 by viforget          #+#    #+#             */
-/*   Updated: 2019/08/06 20:48:37 by viforget         ###   ########.fr       */
+/*   Updated: 2019/08/06 21:18:15 by viforget         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,41 @@ void			aff_error(t_info *tree)
 		put_mult_str(3, "ft_ls: ", tree->name, ": No such file or directory\n");
 	if (tree->right != NULL)
 		aff_error(tree->right);
+}
+
+void			tree_dir(t_info *tree)
+{
+	static int boo = 0;
+	static int boo2 = 0;
+
+	if (tree == NULL)
+		return ;
+	if (boo2 == 0 && tree->left == NULL && tree->right == NULL)
+	{
+		ft_ls(tree->name, "./");
+		return ;
+	}
+	else
+		boo2 = 1;
+	if (tree->left != NULL)
+		tree_dir(tree->left);
+	if (tree->name[0] != '\0')
+	{
+		if (boo == 0)
+		{
+			put_mult_str(2, tree->name, ":\n");
+			ft_ls(tree->name, "./");
+			boo = 1;
+		}
+		else
+		{
+			put_mult_str(3, "\n", tree->name, ":\n");
+			ft_ls(tree->name, "./");
+		}
+
+	}
+	if (tree->right != NULL)
+		tree_dir(tree->right);
 }
 
 static void 	initialize(t_info *tre[3], size_t col[7])
@@ -71,5 +106,7 @@ void			sort_argv(char **argv, int argc, size_t col[7])
 	}
 	add_column(col);
 	aff_error(tre[2]);
-	aff_tree(tre[1], col);
+	if (tre[1] != NULL)
+		aff_tree(tre[1], col);
+	tree_dir(tre[0]);
 }
